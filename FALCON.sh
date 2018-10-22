1f4e71299488f236617088c0ed376aacfb396ccc
mohamad13123331 <42879838+mohamad13123331@users.noreply.github.com>
2018-10-22 22:07:52 +0300

    Update FALCON.sh

--- FALCON.sh
+++ FALCON.sh
@@ -4,14 +4,6 @@ install() {
 wget "https://valtman.name/files/telegram-cli-1222"
 mv telegram-cli-1222 tg
 sudo chmod +x tg
-echo -e "
- __   ___________   ___     _____     __  __
-/  |  |___   ___|  / _ \   | ___ }   |  \/  |
-\_ \      | |     | | | |  | |_) }   | |\/| |
- _) |     | |     | |_| |  |  _< \   | |  | |
-|__/      |_|      \___/   |_|  \_\  |_|  |_|
- BY @TAHAJ20 DEV : TAHAJ20            "
-echo -e "" 
 echo -e "" 
 }
 if [ "$1" = "install" ]; then
